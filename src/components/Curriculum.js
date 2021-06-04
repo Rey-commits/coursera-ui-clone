c37e44c248d32c793db058722eff1bc7871a002f
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-04 14:46:21 +0800

    Row and Cols have been added

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import {Card, Container} from 'react-bootstrap'
+import {Card, Col, Container, Row} from 'react-bootstrap'
 import {curriculumData} from "../curriculumData"
 function Curriculum() {
     return (
         <Container fluid>
-            {curriculumData.map(course => (
-                <div className="curriculum-individual-div">
-                    <Card>
-                        <Card.Img variant="top" src={course.image}/>
-                        <Card.Body>
-                            <Card.Text>
-                                {course.title}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-            ))}
+            <Row xl={2} lg={2} md={2} sm={1} xs={1}>
+                {curriculumData.map(course => (
+                    <Col>
+                        <div className="curriculum-individual-div">
+                            <Card>
+                                <Card.Img variant="top" src={course.image}/>
+                                <Card.Body>
+                                    <Card.Text>
+                                        <h5 className="course-title-text">
+                                            {course.title}
+                                        </h5>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                ))}
+            </Row>
         </Container>
     )
 }
