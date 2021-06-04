178d73e290fd89e6347813f115adb8bcf2e3ed38
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-04 14:46:48 +0800

    curriculum data has been modified

--- src/curriculumData.js
+++ src/curriculumData.js
@@ -1,5 +1,4 @@
-export const curriculumData = [
-    {
+export const curriculumData = [{
         id: 1,
         title: "Responsive Web Design Certification",
         image: "http://www.auburn.edu/outreach/opce/webdesign/images/webDesignCertificateBanner.jpg",
@@ -7,51 +6,51 @@ export const curriculumData = [
     {
         id: 2,
         title: "JavaScript Algorithm and Data Structures Certification",
-        image: "",
+        image: "https://www.luiztools.com.br/wp-content/uploads/2018/09/javascript.png",
     },
     {
         id: 3,
         title: "Front End Libraries Certification",
-        image: "",
+        image: "https://res.cloudinary.com/practicaldev/image/fetch/s--zOM2V229--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/3dso9dv9loxkft0cee4c.jpg",
     },
     {
         id: 4,
         title: "Data Visualization Certification",
-        image: "",
+        image: "https://cdn-images-1.medium.com/max/2000/1*IEwZ5DJLCPmoHPieqTCsdg.png",
     },
     {
         id: 5,
         title: "APIs and Microservices Certification",
-        image: "",
+        image: "https://reg129.imperisoft.com/Pages/System/Image.aspx?id=32076&imgtype=1",
     },
     {
         id: 6,
         title: "Quality Assurance Certification",
-        image: "",
+        image: "https://media.istockphoto.com/photos/quality-assurance-and-quality-control-concept-picture-id1158174820?k=6&m=1158174820&s=612x612&w=0&h=4ycY6uMgzCUI5SmKaAaNAvhK_PjnYUsr6RYm05lRA4k=",
     },
     {
         id: 7,
         title: "Scientific Computing with Python Certification",
-        image: "",
+        image: "https://lh3.googleusercontent.com/proxy/dN3EttNSPUEcCSXMRp30COSZTv1itCkfFOkLgBG_ot3GJ0TjahWWIl_Xc8C1OsWWxez-sGJ-qB9_OpscZGpDLOlMqcSLXliFVpHAxib96WM",
     },
     {
         id: 8,
         title: "Data Analysis with Python Certification",
-        image: "",
+        image: "https://www.syskron.com/fileadmin/_processed_/7/6/csm_datenanalyse-und-monitoring-header-picture-small-20190215_c87bb2f8ff.jpg",
     },
     {
         id: 9,
         title: "Information Security Certification",
-        image: "",
+        image: "https://www.liquidweb.com/static/2da795439f8cc89c865f5305830f8b14/cyber-security-lessons-1.jpg",
     },
     {
         id: 10,
         title: "Machine Learning with Python Certification",
-        image: "",
+        image: "https://www.future-processing.com/blog/wp-content/uploads/2020/03/ai_template.jpg",
     },
     {
         id: 11,
         title: "Coding Interview Prep",
-        image: "",
+        image: "https://superultrawidewallpaper.com/wp-content/uploads/2019/03/codes-3840x1080-1860x523.jpg",
     },
 ]
