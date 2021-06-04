174494f37418158cf163ea6946f84b6033b03224
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-04 13:53:42 +0800

    curriculum data has been added

---
+++ src/curriculumData.js
@@ -0,0 +1,57 @@
+export const curriculumData = [
+    {
+        id: 1,
+        title: "Responsive Web Design Certification",
+        image: "http://www.auburn.edu/outreach/opce/webdesign/images/webDesignCertificateBanner.jpg",
+    },
+    {
+        id: 2,
+        title: "JavaScript Algorithm and Data Structures Certification",
+        image: "",
+    },
+    {
+        id: 3,
+        title: "Front End Libraries Certification",
+        image: "",
+    },
+    {
+        id: 4,
+        title: "Data Visualization Certification",
+        image: "",
+    },
+    {
+        id: 5,
+        title: "APIs and Microservices Certification",
+        image: "",
+    },
+    {
+        id: 6,
+        title: "Quality Assurance Certification",
+        image: "",
+    },
+    {
+        id: 7,
+        title: "Scientific Computing with Python Certification",
+        image: "",
+    },
+    {
+        id: 8,
+        title: "Data Analysis with Python Certification",
+        image: "",
+    },
+    {
+        id: 9,
+        title: "Information Security Certification",
+        image: "",
+    },
+    {
+        id: 10,
+        title: "Machine Learning with Python Certification",
+        image: "",
+    },
+    {
+        id: 11,
+        title: "Coding Interview Prep",
+        image: "",
+    },
+]
