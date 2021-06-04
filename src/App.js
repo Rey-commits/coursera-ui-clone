438f9169e4e66ae2cc934aa38e43af4dd7e46d51
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-04 13:54:41 +0800

    App js has been modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+/* eslint-disable no-unused-vars */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './components/Hero';
 import Navigation from "./components/Navigation";
+import NavTabs from './components/NavTabs';
 import TopicsList from './components/TopicsList';
-
+import Home from './components/Home';
+import Curriculum from './components/Curriculum';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-      <Navigation />
-      <Hero />
-      <TopicsList />
-    </div>
-  )
+    return (
+        <div>
+            <Router>
+                <Navigation/>
+                <Hero/>
+                <NavTabs/>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/tutorials' component={TopicsList}/>
+                    <Route path="/curriculum" component={Curriculum}/>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
