357a369c0fa403779208518131df78a8e0f2f4e6
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-06-04 13:54:07 +0800

    topics data has been added

---
+++ src/topicsData.js
@@ -0,0 +1,51 @@
+export const topicsData = [
+    {
+        id: 1,
+        topic: "Popular Programming Courses",
+        image: "https://cdn.mos.cms.futurecdn.net/ZJdmAC9KRTgY6f7YWkZgRb.jpg",
+        info: "",
+    },
+    {
+        id: 2,
+        topic: "JavaScript Tutorials",
+        image: "https://miro.medium.com/max/1400/1*LyZcwuLWv2FArOumCxobpA.png",
+        info: "",
+
+    },
+    {
+        id: 3,
+        topic: "Python Tutorials",
+        image: "https://cdn.wallpapersafari.com/86/47/beOFkU.jpg",
+        info: "",
+    },
+    {
+        id: 4,
+        topic: "HTML and CSS Tutorials",
+        image: "https://laqshya.in/blog/wp-content/uploads/2020/06/html5-and-css3.png",
+        info: "",
+    },
+    {
+        id: 5,
+        topic: "Data Science",
+        image: "https://www.mccourier.com/wp-content/uploads/2021/03/Online-Data-Science-Training.png",
+        info: "",
+    },
+    {
+        id: 6,
+        topic: "Machine Learning",
+        image: "https://media.vlpt.us/images/kimsj5259/post/ee449c18-c3e3-43e0-abbf-ad227e488414/ML_image.png",
+        info: "",
+    },
+    {
+        id: 7,
+        topic: "Ethical Hacking",
+        image: "https://s27389.pcdn.co/wp-content/uploads/2021/05/how-ethical-hacking-can-improve-your-security-posture-934x440.jpeg",
+        info: "",
+    },
+    {
+        id: 8,
+        topic: "Node.js Tutorials",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhwDztbjy7cu8JusNKcFZLz2hYZgqDrGjsaUs0MWYRJeMO3yFdzbJ5fYRRxGnAUQKmX3Y&usqp=CAU",
+        info: "",
+    },
+]
